2c77392a3ca9ce5fa06ac250bf976d665e7defc4
jceloto7 <jessicaceloto@gmail.com>
2024-07-23 14:06:47 -0300

    improvements

--- Functions/src/main/java/com/jceloto7/Main.java
+++ Functions/src/main/java/com/jceloto7/Main.java
@@ -1,36 +1,31 @@
 package com.jceloto7;
 
-//testando o git
-
 import com.jceloto7.model.Product;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
         Product notebook = new Product("notebook", 2000.75, 0.2);
-        Product PCGamer = new Product("pc gamer",10000,0.1);
+        Product pcGamer = new Product("pc gamer", 10000, 0.1);
 
-        BinaryOperator<Double> multiplication = (x, y) -> (x * y);
-        BinaryOperator<Double> subtraction=(x,y) -> (x-y);
-        BinaryOperator<Double> addition=(x,y) -> (x+y);
-        UnaryOperator<Double> tax = (x) -> (x>=2500 ? x*0.085 :0);
-        UnaryOperator<Double> freight = (x) -> (x>=3000 ? 100.0 : 50.0);
+        Function<Product, Double> priceWithDiscount = product -> product.getPrice() * (1 - product.getDiscount());
+        UnaryOperator<Double> priceWithTax = price -> (price >= 2500 ? price * 1.085 : price);
+        UnaryOperator<Double> priceWithFreight = price -> (price >= 3000 ? price + 100.0 : price + 50.0);
+        Function<Double,String> roundedPrice = price -> String.format("%.2f", price);
+        UnaryOperator<String> priceView = price -> ("R$" + price).replace(".",",");
 
+        Function<Product, String> product = priceWithDiscount
+                .andThen(priceWithTax)
+                .andThen(priceWithFreight)
+                .andThen(roundedPrice)
+                .andThen(priceView);
 
-        Consumer<Product> finalRealPrice = product ->{
-            Double discountResult= multiplication.apply(product.getPrice(), product.getDiscount());
-            Double finalPrice=subtraction.apply(product.getPrice(), discountResult);
-            Double priceTax = tax.apply(finalPrice);
-            Double finalTax = addition.apply(finalPrice, priceTax);
-            Double priceFreight = freight.apply(finalTax);
-            Double finalFreight = addition.apply(finalTax, priceFreight);
-            System.out.printf("R$" + "%.2f%n",finalFreight);
-        };
-
-        finalRealPrice.accept(notebook);
-        finalRealPrice.accept(PCGamer);
-
+        System.out.println(product.apply(notebook));
+        System.out.println(product.apply(pcGamer));
     }
 }
+
+
+
+
